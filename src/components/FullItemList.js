c0c8aded98d374848ac72c59493ba1ca11eea270
ttx89 <91700476+ttx89@users.noreply.github.com>
2022-07-23 17:24:36 -0400

    uninstall react-table-6

--- src/components/FullItemList.js
+++ src/components/FullItemList.js
@@ -1,135 +1,135 @@
-import React, { useState, useEffect, useContext } from "react";
-import { ThemeContext } from '../context';
+// import React, { useState, useEffect, useContext } from "react";
+// import { ThemeContext } from '../context';
 
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 
-import { Button } from "react-bootstrap";
-import ItemTableRow from "./ItemTableRow";
-import ReactTable from "react-table-6";
-import "react-table-6/react-table.css";
-import "../styles/itemlist_g.css";
+// import { Button } from "react-bootstrap";
+// import ItemTableRow from "./ItemTableRow";
+// import ReactTable from "react-table-6";
+// import "react-table-6/react-table.css";
+// import "../styles/itemlist_g.css";
 
-const FullItemList = () => {
-  const url = process.env.REACT_APP_SERVER_URL;
-  const navigate = useNavigate();
+// const FullItemList = () => {
+//   const url = process.env.REACT_APP_SERVER_URL;
+//   const navigate = useNavigate();
 
 
-  const [items, setItems] = useState([]);
+//   const [items, setItems] = useState([]);
 
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
+//   const theme = useContext(ThemeContext);
+//   const darkMode = theme.state.darkMode;
 
-  useEffect(() => {
-    axios
-    .get(url + "/items")
-      // .get("http://localhost:3001/items/")
-      .then(({ data }) => {
-        setItems(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+//   useEffect(() => {
+//     axios
+//     .get(url + "/items")
+//       // .get("http://localhost:3001/items/")
+//       .then(({ data }) => {
+//         setItems(data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
 
-  const DataTable = () => {
-    return items.map((res, i) => {
-      return <ItemTableRow obj={res} key={i} />;
-    });
-  };
+//   const DataTable = () => {
+//     return items.map((res, i) => {
+//       return <ItemTableRow obj={res} key={i} />;
+//     });
+//   };
 
-  const itemDetails = (_id) => {
-    navigate("/details/" + _id);
-  };
+//   const itemDetails = (_id) => {
+//     navigate("/details/" + _id);
+//   };
 
-  return (
-    <div className="item-container">
-      <div className="table-wrapper">
-        <ReactTable
-          className="react-table"
-          style={{
-            color: darkMode && "white",
-          }}
-          data={items}
-          defaultPageSize={8}
-          pageSizeOptions={[5, 8, 10, 20]}
-          filterable
-          defaultFilterMethod={(filter, row) =>
-            String(row[filter.id]) === filter.value
-          }
-          columns={[
-            {
-              Header: "ItemName",
-              accessor: "itemname",
-              filterMethod: (filter, row) =>
-                row[filter.id].startsWith(filter.value),
-            },
-            {
-              Header: "Restaurant Name",
-              accessor: "username",
-              filterMethod: (filter, row) =>
-                row[filter.id].startsWith(filter.value),
-            },
-            {
-              Header: "Expire Date",
-              accessor: "expiredate",
-              filterMethod: (filter, row) =>
-                row[filter.id].startsWith(filter.value),
-            },
-            {
-              Header: "Address",
-              accessor: "address",
-              filterMethod: (filter, row) =>
-                row[filter.id].startsWith(filter.value),
-            },
-            {
-              Header: "Description",
-              accessor: "description",
-              filterMethod: (filter, row) =>
-                row[filter.id].startsWith(filter.value),
-            },
-            {
-              Header: "Action",
+//   return (
+//     <div className="item-container">
+//       <div className="table-wrapper">
+//         <ReactTable
+//           className="react-table"
+//           style={{
+//             color: darkMode && "white",
+//           }}
+//           data={items}
+//           defaultPageSize={8}
+//           pageSizeOptions={[5, 8, 10, 20]}
+//           filterable
+//           defaultFilterMethod={(filter, row) =>
+//             String(row[filter.id]) === filter.value
+//           }
+//           columns={[
+//             {
+//               Header: "ItemName",
+//               accessor: "itemname",
+//               filterMethod: (filter, row) =>
+//                 row[filter.id].startsWith(filter.value),
+//             },
+//             {
+//               Header: "Restaurant Name",
+//               accessor: "username",
+//               filterMethod: (filter, row) =>
+//                 row[filter.id].startsWith(filter.value),
+//             },
+//             {
+//               Header: "Expire Date",
+//               accessor: "expiredate",
+//               filterMethod: (filter, row) =>
+//                 row[filter.id].startsWith(filter.value),
+//             },
+//             {
+//               Header: "Address",
+//               accessor: "address",
+//               filterMethod: (filter, row) =>
+//                 row[filter.id].startsWith(filter.value),
+//             },
+//             {
+//               Header: "Description",
+//               accessor: "description",
+//               filterMethod: (filter, row) =>
+//                 row[filter.id].startsWith(filter.value),
+//             },
+//             {
+//               Header: "Action",
 
-              accessor: "isActive",
-              id: "over",
-              Cell: (data) => (
-                <div>
-                  <Button
-                    size="sm"
-                    onClick={(props) => itemDetails(data.row._original._id)}
-                    variant="primary"
-                  >
-                    Details
-                  </Button>
-                </div>
-              ),
-              filterMethod: (filter, row) => {
-                if (filter.value === "all") {
-                  return true;
-                }
-                if (filter.value === "true") {
-                  return row[filter.id];
-                }
-                return !row[filter.id];
-              },
-              Filter: ({ filter, onChange }) => (
-                <select
-                  onChange={(event) => onChange(event.target.value)}
-                  style={{ width: "100%" }}
-                  value={filter ? filter.value : "all"}
-                >
-                  <option value="all">Show All</option>
-                  <option value="true">Ungrabbed</option>
-                  <option value="false">Grabbed</option>
-                </select>
-              ),
-            },
-          ]}
-        />
-      </div>
-    </div>
-  );
-};
+//               accessor: "isActive",
+//               id: "over",
+//               Cell: (data) => (
+//                 <div>
+//                   <Button
+//                     size="sm"
+//                     onClick={(props) => itemDetails(data.row._original._id)}
+//                     variant="primary"
+//                   >
+//                     Details
+//                   </Button>
+//                 </div>
+//               ),
+//               filterMethod: (filter, row) => {
+//                 if (filter.value === "all") {
+//                   return true;
+//                 }
+//                 if (filter.value === "true") {
+//                   return row[filter.id];
+//                 }
+//                 return !row[filter.id];
+//               },
+//               Filter: ({ filter, onChange }) => (
+//                 <select
+//                   onChange={(event) => onChange(event.target.value)}
+//                   style={{ width: "100%" }}
+//                   value={filter ? filter.value : "all"}
+//                 >
+//                   <option value="all">Show All</option>
+//                   <option value="true">Ungrabbed</option>
+//                   <option value="false">Grabbed</option>
+//                 </select>
+//               ),
+//             },
+//           ]}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default FullItemList;
+// export default FullItemList;
